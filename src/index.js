ded6fd7080d0bded7659ba78590649e0702c1459
Arjana <arjana99patel@zohomail.in>
2020-09-01 22:08:55 +0530

    Added simple ex for reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer'
 import AppRoutes from './routes';
 
+const store = createStore(rootReducer)
 
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
+ReactDOM.render( 
+    <Provider store={store}>
+        <AppRoutes/>
+   </Provider>,
+    document.getElementById('root')
+);
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 class Routes extends Component{
 
     render(){
