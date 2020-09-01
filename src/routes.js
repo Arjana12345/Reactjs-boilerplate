cb6eea731d132d5a6b9c451658fd1d388d17abd2
Arjana <arjana99patel@zohomail.in>
2020-09-01 12:56:15 +0530

    Added src folder to print

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Routes extends Component{
+
+    render(){
+        return(
+           <div>
+              <h1>First React App</h1>
+           </div>
+        );
+     }
+} 
+
+export default Routes;
