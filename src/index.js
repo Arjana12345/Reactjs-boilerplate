60b5331465fabf0ef3809b20613dd96e5909344a
Arjana <arjana99patel@zohomail.in>
2020-09-02 17:40:07 +0530

    Creating Store and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux'
+// import rootReducer from './reducers/rootReducer'
+
+
+// const store = createStore(rootReducer)
+
+// ReactDOM.render( 
+//     <Provider store={store}>
+//         <AppRoutes/>
+//    </Provider>,
+//     document.getElementById('root')
+// );
+
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
+import loggerMiddleware from './middleware/logger'
+import monitorReducerEnhancer from './enhancers/monitorReducer'
 import AppRoutes from './routes';
 
-const store = createStore(rootReducer)
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
 
-ReactDOM.render( 
+const store = createStore(rootReducer, undefined, composedEnhancers)
+
+render( 
     <Provider store={store}>
         <AppRoutes/>
    </Provider>,
