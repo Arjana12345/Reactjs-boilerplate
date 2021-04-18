301fcae16b5a6e3d033c8bd642f790d6963aa004
Arjana <arjana99patel@zohomail.in>
2021-04-18 19:52:00 +0530

    graphql-example

--- backend/test.php
+++ backend/test.php
@@ -1,5 +1,7 @@
 <?php
- header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 $test = array(array(
                                 "name"=> "Arjana",
                                 "lastName" => "Haldkar"
@@ -11,6 +13,21 @@ $test = array(array(
     
             );
 
-echo json_encode($test);
+//echo json_encode($test);
+//echo $_POST['id'];
+//echo '{ "data": { "user": { "id":"1","name": "arju" } } }';
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == 'POST')
+{
+    $myObj->data = ($_POST);
+
+    print_r(json_encode($myObj));
+}else{
+    echo '{ "data": { "user": { "id":"1","name": "arju" } } }';
+}
+
+
 
 ?>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 import AppRoutes from './routes';
 
@@ -14,6 +15,20 @@ const store = createStore(
   rootReducer,
   applyMiddleware(thunk,logger)
 )
+
+const client = new ApolloClient({
+  uri: 'http://localhost/reactApp/firstReactApp/backend/test.php',
+  cache: new InMemoryCache()
+});
+
+client.query({ query: gql`
+  {
+    user(id: 1) {
+      id
+      name
+    }
+  }
+`}).then(result =>console.log(result));
 
 
 render( 
